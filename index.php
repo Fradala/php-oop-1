944d9e461dfbe90af3fd2c93875708b4b7bc3301
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2024-01-25 14:54:49 +0100

    creata funzione  construct

--- index.php
+++ index.php
@@ -4,4 +4,13 @@ class Movie {
     public $title;
     public $genre;
     public $releaseYear;
+
+
+    public function __construct(string $_title, string $_genre, float $_releaseYear){
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->releaseYear = $_releaseYear;
+    }
+
+    
 }
